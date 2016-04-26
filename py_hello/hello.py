9f9fb42ad0d0411febb62ec79b86a62150d574d8
ivan0124 <ivan0124.tw@gmail.com>
2016-04-26 06:56:50 +0000

    modify- update hello.py

--- py_hello/hello.py
+++ py_hello/hello.py
@@ -3,6 +3,17 @@
 def main():
     print "Hello, World!"
 
+    #input is nubmer
+    res=foo(10,5)
+    print "res number=%d" % (res)
+    
+    #input is string
+    res=foo("777","333")
+    print "res string=%s" % (res)
+
+def foo(p1, p2):
+    res=p1+p2
+    return res
 
 if __name__  == "__main__":
     main()
