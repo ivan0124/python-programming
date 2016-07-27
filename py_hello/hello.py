3cc93aaaab30f2021ad1cb5f992939da715623e3
ivan0124 <ivan0124.tw@gmail.com>
2016-07-27 10:55:25 +0800

    fix print error

--- py_hello/hello.py
+++ py_hello/hello.py
@@ -3,24 +3,24 @@
 import library as lib
 
 def main():
-    print "Hello, World!"
+    print ("Hello, World!")
 
     #input is nubmer
     res=lib.funA(10,5)
-    print "res number=%d" % (res)
+    print ("res number=%d" % (res))
     
     #input is string
     res=foo("777","333")
-    print "res string=%s" % (res)
+    print ("res string=%s" % (res))
 
     for i in range(1,10):
-        print "i=%d" % i
+        print ("i=%d" % i)
 
 def foo(p1, p2):
     res=p1+p2
     return res
 
-print "__name__ value is %s" % (__name__)
+print ("__name__ value is %s" % (__name__))
 
 if __name__  == "__main__":
     main()
--- py_hello/library.py
+++ py_hello/library.py
@@ -1,15 +1,15 @@
 #!/usr/bin/python
 
 def main():
-    print "library.py running!"
+    print ("library.py running!")
 
 def funA(p1, p2):
     res=p1+p2
     return res
 
-print "__name__ value is %s" % (__name__)
+print ("__name__ value is %s" % (__name__))
 
 if __name__  == "__main__":
     main()
 else:
-    print "library.py running as module"
+    print ("library.py running as module")
