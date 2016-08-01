24f5c0c1f3b24e0e706d2eb00da350d90eb27a84
ivan0124 <ivan0124.tw@gmail.com>
2016-08-01 13:45:16 +0800

    read and plot data success

--- py_test/hello.py
+++ py_test/hello.py
@@ -1,10 +1,26 @@
 #!/usr/bin/python
 
 import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
 
 def main():
     print ("Hello, World!")
     df = pd.read_csv('./iris.data',header=None)
+    df.tail();
+
+    y = df.iloc[0:99, 4].values
+    y = np.where(y == 'Iris-setosa', -1, 1)
+    x = df.iloc[0:99, [0,2]].values
+    print(x);
+    plt.scatter(x[:49, 0], x[:49, 1], color='red', marker='o', label='setosa')
+    plt.scatter(x[49:99, 0], x[49:99, 1], color='blue', marker='x', label='versicolor')
+
+    plt.xlabel('petal length')
+    plt.ylabel('sepal length')
+    plt.legend(loc='upper left')
+
+    plt.show()
 
 print ("__name__ value is %s" % (__name__))
 
