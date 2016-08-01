91304a05dc50c716260263e5c1f76c4827cad856
ivan0124 <ivan0124.tw@gmail.com>
2016-08-01 14:27:34 +0800

    add plot trainning

--- py_test/hello.py
+++ py_test/hello.py
@@ -3,6 +3,33 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
+#import perceptron as per
+
+class Perceptron(object):
+
+   def __init__(self, eta=0.01, n_iter=10):
+      self.eta = eta
+      self.n_iter = n_iter
+   
+   def fit(self, X, y):
+      self.w_ = np.zeros(1 + X.shape[1])
+      self.errors_ = []
+  
+      for _ in range(self.n_iter):
+          errors = 0
+          for xi, target in zip(X,y):
+              update = self.eta * (target - self.predict(xi))
+              self.w_[1:] += update * xi
+              self.w_[0] += update
+              errors += int(update != 0.0)
+          self.errors_.append(errors)
+      return self
+
+   def net_input(self, X):
+       return np.dot(X, self.w_[1:]) + self.w_[0]
+
+   def predict(self, X):
+       return np.where(self.net_input(X) >= 0.0, 1, -1)
 
 def main():
     print ("Hello, World!")
@@ -11,17 +38,25 @@ def main():
 
     y = df.iloc[0:99, 4].values
     y = np.where(y == 'Iris-setosa', -1, 1)
-    x = df.iloc[0:99, [0,2]].values
-    print(x);
-    plt.scatter(x[:49, 0], x[:49, 1], color='red', marker='o', label='setosa')
-    plt.scatter(x[49:99, 0], x[49:99, 1], color='blue', marker='x', label='versicolor')
+    X = df.iloc[0:99, [0,2]].values
+    '''
+    print(X);
+    plt.scatter(X[:49, 0], X[:49, 1], color='red', marker='o', label='setosa')
+    plt.scatter(X[49:99, 0], X[49:99, 1], color='blue', marker='x', label='versicolor')
 
     plt.xlabel('petal length')
     plt.ylabel('sepal length')
     plt.legend(loc='upper left')
 
     plt.show()
-
+    '''
+    ppn = Perceptron(eta=0.1, n_iter=10) 
+    ppn.fit(X,y)  
+    plt.plot(range(1, len(ppn.errors_) + 1), ppn.errors_, marker='o')
+    plt.xlabel('Epochs')
+    plt.ylabel('Number of misclassifications')
+    plt.show()
+ 
 print ("__name__ value is %s" % (__name__))
 
 if __name__  == "__main__":
