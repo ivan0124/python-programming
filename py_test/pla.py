cd8e65b976bd114f39ef285f04c4340c66d10b26
ivan0124 <ivan0124.tw@gmail.com>
2016-08-03 10:37:27 +0800

    add get_w function

--- py_test/pla.py
+++ py_test/pla.py
@@ -28,6 +28,9 @@ class Perceptron(object):
    def predict(self, X):
        return np.where(self.net_input(X) >= 0.0, 1, -1)
 
+   def get_w(self):
+       return self.w_
+
 def main():
     print ("Hello, pla.py!")
  
