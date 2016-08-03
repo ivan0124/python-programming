9e9a3941e8a950821f6bd86e8fe370a4ba6f110d
ivan0124 <ivan0124.tw@gmail.com>
2016-08-03 09:01:47 +0800

    remove unused code

--- py_test/hello.py
+++ py_test/hello.py
@@ -6,33 +6,6 @@ import numpy as np
 from matplotlib.colors import ListedColormap
 from pla import Perceptron
 
-'''
-class Perceptron(object):
-
-   def __init__(self, eta=0.01, n_iter=10):
-      self.eta = eta
-      self.n_iter = n_iter
-   
-   def fit(self, X, y):
-      self.w_ = np.zeros(1 + X.shape[1])
-      self.errors_ = []
-  
-      for _ in range(self.n_iter):
-          errors = 0
-          for xi, target in zip(X,y):
-              update = self.eta * (target - self.predict(xi))
-              self.w_[1:] += update * xi
-              self.w_[0] += update
-              errors += int(update != 0.0)
-          self.errors_.append(errors)
-      return self
-
-   def net_input(self, X):
-       return np.dot(X, self.w_[1:]) + self.w_[0]
-
-   def predict(self, X):
-       return np.where(self.net_input(X) >= 0.0, 1, -1)
-'''
 def plot_decision_regions(X, y, classifier, resolution=0.02): 
     markers = ('s', 'x', 'o', '^', 'v')
     colors = ('red', 'blue', 'lightgreen', 'gary', 'cyan')
