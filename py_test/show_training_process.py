d421b99885f5d3b84fc205c21f385f5d85e5f701
ivan0124 <ivan0124.tw@gmail.com>
2016-08-03 13:49:04 +0800

    update file

--- py_test/show_training_process.py
+++ py_test/show_training_process.py
@@ -3,6 +3,7 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
+import configparser
 from pla import Perceptron
 
 
@@ -16,8 +17,18 @@ def main():
     X = df.iloc[0:99, [0,2]].values
 
     ppn = Perceptron(eta=0.1, n_iter=10) 
+    #training data
     ppn.fit(X,y)
+    weight=ppn.get_w();
 
+    #save weight to file
+    config = configparser.ConfigParser()
+    config['DEFAULT']['weight'] = repr(weight)    # update
+
+    with open('FILE.INI', 'w') as configfile:    # save
+        config.write(configfile)
+
+    #plot 
     plt.plot(range(1, len(ppn.errors_) + 1), ppn.errors_, marker='o')
     plt.xlabel('Epochs')
     plt.ylabel('Number of misclassifications')
