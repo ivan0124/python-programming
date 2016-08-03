7748c503d96aa4f4ab5d175dc5d92f573e34e1d5
ivan0124 <ivan0124.tw@gmail.com>
2016-08-03 08:54:21 +0800

    add pla.py

--- py_test/pla.py
+++ py_test/pla.py
@@ -4,9 +4,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
 from matplotlib.colors import ListedColormap
-from pla import Perceptron
 
-'''
 class Perceptron(object):
 
    def __init__(self, eta=0.01, n_iter=10):
@@ -55,41 +53,9 @@ def plot_decision_regions(X, y, classifier, resolution=0.02):
         plt.scatter(x=X[y == cl, 0], y=X[y == cl, 1],
                     alpha=0.8, c=cmap(idx),
                     marker=markers[idx], label=cl)
-
+'''
 def main():
-    print ("Hello, World!")
-    df = pd.read_csv('./iris.data',header=None)
-    df.tail();
-
-    y = df.iloc[0:99, 4].values
-    y = np.where(y == 'Iris-setosa', -1, 1)
-    X = df.iloc[0:99, [0,2]].values
-    '''
-    print(X);
-    plt.scatter(X[:49, 0], X[:49, 1], color='red', marker='o', label='setosa')
-    plt.scatter(X[49:99, 0], X[49:99, 1], color='blue', marker='x', label='versicolor')
-
-    plt.xlabel('petal length')
-    plt.ylabel('sepal length')
-    plt.legend(loc='upper left')
-
-    plt.show()
-    '''
-
-    ppn = Perceptron(eta=0.1, n_iter=10) 
-    ppn.fit(X,y) 
-    '''
-    plt.plot(range(1, len(ppn.errors_) + 1), ppn.errors_, marker='o')
-    plt.xlabel('Epochs')
-    plt.ylabel('Number of misclassifications')
-    plt.show()
-    '''
-     
-    plot_decision_regions(X, y, classifier=ppn)
-    plt.xlabel('sepal length [cm]')
-    plt.ylabel('petal length [cm]')
-    plt.legend(loc='upper left')
-    plt.show();        
+    print ("Hello, pla.py!")
  
 print ("__name__ value is %s" % (__name__))
 
