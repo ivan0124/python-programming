2d3f5bc3d5ecd2e6c9db15d7d3e479033fe4a0df
ivan0124 <ivan0124.tw@gmail.com>
2016-08-04 14:14:20 +0800

    update file

--- py_test/pla.py
+++ py_test/pla.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 
 import numpy as np
+import configparser
 
 class Perceptron(object):
 
@@ -34,10 +35,44 @@ class Perceptron(object):
    def set_w(self, w):
        self.w_ = w
 
+def set_weight(config_path_name):
+
+    config = configparser.ConfigParser()
+    config.read(config_path_name)
+    print(config['DEFAULT']['weight'])
+    a=config['DEFAULT']['weight']
+
+    print("read FILE.INI weight =%s" % a)
+    floats = map(float, a.split(','))
+
+    print('------------');
+    print("set perceptron weight  = %s" % floats)
+    ppn.set_w( np.array(floats))
+    Weight=ppn.get_w();
+    print("get perceptron weight  = %s" % Weight)
+    print("----------")
+    return 0
+
+def predict(X):
+    print("predict")
+    print("----------")
+    floats = map(float, X.split(','))
+    X_point=np.array(floats)
+    predict=ppn.predict(X_point);
+    print("X_point = %s, predict result = %d" % (X_point,predict))
+    return predict
+
+
 def main():
-    print ("Hello, pla.py!")
+    print ("Hello, pla.py main() run!")
+    set_weight("FILE.INI")
+    predict("5,3")
  
 print ("__name__ value is %s" % (__name__))
 
 if __name__  == "__main__":
+    ppn = Perceptron(eta=0.3, n_iter=10)
     main()
+else:
+    print("pla.py is loaded.");
+    ppn = Perceptron(eta=0.3, n_iter=10)
