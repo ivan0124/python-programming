7be91d07109e46d628242d30f2ec8c5b2632afe7
ivan0124 <ivan0124.tw@gmail.com>
2016-08-31 11:35:52 +0800

    update code

--- scikit_KNN/scikit_knn.py
+++ scikit_KNN/scikit_knn.py
@@ -7,7 +7,7 @@ import matplotlib.pyplot as plt
 from sklearn import datasets
 from sklearn.cross_validation import train_test_split
 from sklearn.preprocessing import StandardScaler
-from sklearn.svm import SVC
+from sklearn.neighbors import KNeighborsClassifier
 from sklearn.metrics import accuracy_score
 
 def plot_decision_regions(X, y, classifier, test_idx=None, resolution=0.02): 
@@ -61,11 +61,11 @@ def main():
     X_test_std = sc.transform(X_test)
    
     #trainning model
-    svm = SVC(kernel='rbf', random_state=0, gamma=0.1, C=10.0)
-    svm.fit(X_train_std, y_train) 
+    knn = KNeighborsClassifier(n_neighbors=5, p=2, metric='minkowski')
+    knn.fit(X_train_std, y_train) 
 
     #predict
-    y_pred = svm.predict(X_test_std);
+    y_pred = knn.predict(X_test_std);
     print("Misclassified samples: %d" %(y_test != y_pred).sum()) 
     
     #Accuracy
@@ -78,7 +78,7 @@ def main():
     y_combined = np.hstack((y_train, y_test))
     plot_decision_regions(X=X_combined_std,
                           y=y_combined,
-                          classifier=svm,
+                          classifier=knn,
                           test_idx=range(105,150))
     plt.xlabel('petal length [standardized]')
     plt.ylabel('petal width [standardized]')
