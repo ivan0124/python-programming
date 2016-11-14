c0b8cb2c601521c3e109ded7dbbf28545b45d35f
ivan0124 <ivan0124.tw@gmail.com>
2016-11-14 09:38:17 +0800

    update code

--- TF_QA_HDD/TF_HDD_prediction.py
+++ TF_QA_HDD/TF_HDD_prediction.py
@@ -4,24 +4,35 @@ import tensorflow as tf
 import numpy as np
 import pandas as pd
 
+# Train parameters
+test_sample_rate = 0.3
+train_label_column = [0] 
+train_feature_column = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16] 
+train_feature_num = len (train_feature_column)
+train_classes_num = 2 
+train_num_trees=5
+train_max_nodes=1000
+train_steps=300
+
+
 # Load datasets.
 ipd = pd.read_csv("./HDD_SMART_DATA.csv")
-shuffled = ipd.sample(frac=1)
 
-x_test_sample_num = int(len(shuffled) * 0.3)
-print ("x_test_len = %s\n" % (x_test_sample_num))
-x_train = shuffled.values[0:len(shuffled)- x_test_sample_num,[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16]]
-y_train = shuffled.values[0:len(shuffled)- x_test_sample_num,[0]]
-x_test = shuffled.values[len(shuffled)- x_test_sample_num:,[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16]]
-y_test = shuffled.values[len(shuffled)- x_test_sample_num:,[0]]
+# Shuffle datasets
+shuffled = ipd.sample(frac=1)
+x_test_sample_num = int(len(shuffled) * test_sample_rate)
+x_train = shuffled.values[0:len(shuffled)- x_test_sample_num, train_feature_column]
+y_train = shuffled.values[0:len(shuffled)- x_test_sample_num, train_label_column]
+x_test = shuffled.values[len(shuffled)- x_test_sample_num:, train_feature_column]
+y_test = shuffled.values[len(shuffled)- x_test_sample_num:, train_label_column]
 
 # Build random forest model.
 hparams = tf.contrib.tensor_forest.python.tensor_forest.ForestHParams(
-        num_trees=5, max_nodes=1000, num_classes=2, num_features=16)
+        num_trees=train_num_trees, max_nodes=train_max_nodes, num_classes=train_classes_num, num_features=train_feature_num)
 classifier = tf.contrib.learn.TensorForestEstimator(hparams)
 
 # Fit model.
-classifier.fit(x=x_train, y=y_train, steps=300)
+classifier.fit(x=x_train, y=y_train, steps=train_steps)
 
 # Evaluate accuracy.
 accuracy_score = classifier.evaluate(x=x_test, y=y_test)["accuracy"]
