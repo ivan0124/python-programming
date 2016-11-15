25c9e60d774b30ecd8c9c93139043aa0b1306bd3
ivan0124 <ivan0124.tw@gmail.com>
2016-11-15 10:21:00 +0800

    update code

---
+++ py_csv_fill0/fill0.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python
+
+import csv
+import numpy as np
+import pandas as pd
+
+def main():
+
+     with open("./hdd.csv","rb") as source:
+	 rdr= csv.reader( source )
+         #for row in rdr:
+         #    for i, x in enumerate(row):
+         #        if len(x)< 1:
+         #            x = row[i] = 0
+         #        print x
+         with open("result.cvs","wb") as result:
+             wtr= csv.writer( result )
+             for r in rdr:
+                 for i, x in enumerate(r):
+                     if len(x)< 1:
+                         r[i] = 0
+                 wtr.writerow( (r[0], r[1], r[2], r[3], r[4], r[5], r[6], r[7],r[8]) )
+ 
+
+print "__name__ value is %s" % (__name__)
+
+if __name__  == "__main__":
+    main()
