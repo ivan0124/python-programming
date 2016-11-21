8a0f68c04dfb5a6d6c9bc3a99a8cdf667270c0b2
ivan0124 <ivan0124.tw@gmail.com>
2016-11-21 13:15:57 +0800

    update code

---
+++ py_if_else/test_if.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+
+def main():
+
+    var1 = 100
+    if var1 > 1:
+        print "1 - Got a true expression value"
+        print var1
+    else:
+        print "1 - Got a false expression value"
+        print var1
+
+if __name__  == "__main__":
+    main()
