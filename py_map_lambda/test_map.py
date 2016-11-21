f660e1a7ad587c4fd3bcca2d9a45dcdd881beee6
ivan0124 <ivan0124.tw@gmail.com>
2016-11-21 13:40:33 +0800

    update code

--- py_map_lambda/test_map.py
+++ py_map_lambda/test_map.py
@@ -16,6 +16,18 @@ def main():
     my_y1 = map( lambda x : x*x, my_y1 )
     print ("map my_y1 = %s \n" % my_y1)
 
+    ############################
+    my_y2 = [[0.7, 0.2], [0.1,0.8], [0.6, 0.4]]
+
+    for i in range(len(my_y2)):
+        if my_y2[i][0] > 0.618:
+            my_y2[i][0] = 1
+            my_y2[i][1] = 0
+        else:
+            my_y2[i][0] = 0
+            my_y2[i][1] = 1
+
+    print ("map my_y2 = %s \n" % my_y2)
 
 if __name__  == "__main__":
     main()
