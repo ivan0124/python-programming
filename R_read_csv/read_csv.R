0b199ec107716a442d2d7c88eeea6672698fe183
ivan0124 <ivan0124.tw@gmail.com>
2019-03-12 18:28:58 +0800

    add histogram plot

--- R_read_csv/read_csv.R
+++ R_read_csv/read_csv.R
@@ -14,3 +14,57 @@ iris=read.csv('./iris.csv')
 
 # First let's get a random sampling of the data
 iris[sample(nrow(iris),10),]
+
+# Density & Frequency analysis with the Histogram,
+
+# Sepal length 
+HisSl <- ggplot(data=iris, aes(x=sepal_length))+
+  geom_histogram(binwidth=0.2, color="black", aes(fill=Species)) + 
+  xlab("Sepal Length (cm)") +  
+  ylab("Frequency") + 
+  theme(legend.position="none")+
+  ggtitle("Histogram of Sepal Length")+
+  geom_vline(data=iris, aes(xintercept = mean(sepal_length)),linetype="dashed",color="grey")
+
+
+# Sepal width
+HistSw <- ggplot(data=iris, aes(x=sepal_width)) +
+  geom_histogram(binwidth=0.2, color="black", aes(fill=Species)) + 
+  xlab("Sepal Width (cm)") +  
+  ylab("Frequency") + 
+  theme(legend.position="none")+
+  ggtitle("Histogram of Sepal Width")+
+  geom_vline(data=iris, aes(xintercept = mean(sepal_width)),linetype="dashed",color="grey")
+
+
+# Petal length
+HistPl <- ggplot(data=iris, aes(x=petal_length))+
+  geom_histogram(binwidth=0.2, color="black", aes(fill=Species)) + 
+  xlab("Petal Length (cm)") +  
+  ylab("Frequency") + 
+  theme(legend.position="none")+
+  ggtitle("Histogram of Petal Length")+
+  geom_vline(data=iris, aes(xintercept = mean(petal_length)),
+             linetype="dashed",color="grey")
+
+
+
+# Petal width
+HistPw <- ggplot(data=iris, aes(x=petal_width))+
+  geom_histogram(binwidth=0.2, color="black", aes(fill=Species)) + 
+  xlab("Petal Width (cm)") +  
+  ylab("Frequency") + 
+  theme(legend.position="right" )+
+  ggtitle("Histogram of Petal Width")+
+  geom_vline(data=iris, aes(xintercept = mean(petal_width)),linetype="dashed",color="grey")
+
+
+# Plot all visualizations
+grid.arrange(HisSl + ggtitle(""),
+             HistSw + ggtitle(""),
+             HistPl + ggtitle(""),
+             HistPw  + ggtitle(""),
+             nrow = 2,
+             top = textGrob("Iris Frequency Histogram", 
+                            gp=gpar(fontsize=15))
+)
