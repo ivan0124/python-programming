a96b936274d16d15b09337a241ebff10a4270a4c
ivan0124 <ivan0124.tw@gmail.com>
2019-03-14 23:15:37 +0800

    add R_box_plot

---
+++ R_box_plot/README.md
@@ -0,0 +1,6 @@
+Step1: **Install R and R Studio**
+* [How to install R and R Studio](https://github.com/ivan0124/python-programming/wiki/How-to-install-R-and-R-Studio)
+
+Step2: **Launch R script**
+* `$Rscript read_csv.R`
+
---
+++ R_box_plot/box_plot.R
@@ -0,0 +1,60 @@
+# This R environment comes with all of CRAN preinstalled, as well as many other helpful packages
+# The environment is defined by the kaggle/rstats docker image: https://github.com/kaggle/docker-rstats
+# For example, here's several helpful packages to load in 
+
+library(ggplot2) # Data visualization
+library(readr) # CSV file I/O, e.g. the read_csv function
+library(gridExtra)
+library(grid)
+library(plyr)
+
+# Load the dataset
+iris=read.csv('/home/ivan/python-programming/R_box_plot/iris.csv')
+
+
+# First let's get a random sampling of the data
+iris[sample(nrow(iris),10),]
+
+# Notice the shape of the data, most attributes exhibit a normal distribution. 
+# You can see the measurements of very small flowers in the Petal width and length column.
+
+
+# Let's plot all the variables in a single visualization that will contain all the boxplots
+
+
+BpSl <- ggplot(iris, aes(Species, sepal_length, fill=Species)) + 
+  geom_boxplot()+
+  scale_y_continuous("Sepal Length (cm)", breaks= seq(0,30, by=.5))+
+  theme(legend.position="none")
+
+
+
+BpSw <-  ggplot(iris, aes(Species, sepal_width, fill=Species)) + 
+  geom_boxplot()+
+  scale_y_continuous("Sepal Width (cm)", breaks= seq(0,30, by=.5))+
+  theme(legend.position="none")
+
+
+
+BpPl <- ggplot(iris, aes(Species, petal_length, fill=Species)) + 
+  geom_boxplot()+
+  scale_y_continuous("Petal Length (cm)", breaks= seq(0,30, by=.5))+
+  theme(legend.position="none")
+
+
+
+BpPw <-  ggplot(iris, aes(Species, petal_width, fill=Species)) + 
+  geom_boxplot()+
+  scale_y_continuous("Petal Width (cm)", breaks= seq(0,30, by=.5))+
+  labs(title = "Iris Box Plot", x = "Species")
+
+# Plot all visualizations
+grid.arrange(BpSl  + ggtitle(""),
+             BpSw  + ggtitle(""),
+             BpPl + ggtitle(""),
+             BpPw + ggtitle(""),
+             nrow = 2,
+             top = textGrob("Sepal and Petal Box Plot", 
+                            gp=gpar(fontsize=15))
+)
+
