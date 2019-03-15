e45e5cf70f5790b9f1bb14223fd9df4c2c1dadc8
ivan0124 <ivan0124.tw@gmail.com>
2019-03-15 17:08:50 +0800

    add R_qcc_xbar_plot

---
+++ R_qcc_xbar_plot/xbar_plot.R
@@ -0,0 +1,22 @@
+# This R environment comes with all of CRAN preinstalled, as well as many other helpful packages
+# The environment is defined by the kaggle/rstats docker image: https://github.com/kaggle/docker-rstats
+# For example, here's several helpful packages to load in 
+
+#library(ggplot2) # Data visualization
+library(readr) # CSV file I/O, e.g. the read_csv function
+#library(gridExtra)
+#library(grid)
+#library(plyr)
+library(qcc)
+
+
+# Load the dataset
+pistonrings=read.csv('/home/ivan/python-programming/R_qcc_xbar_plot/pistonring.csv')
+
+# List data
+head(pistonrings,200)
+#
+diameter = with(pistonrings, qcc.groups(diameter, sample))
+head(diameter,n=100)
+q1 = qcc(diameter[1:25,], type="xbar", newdata=diameter[26:40,])
+
